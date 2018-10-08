201fd7a691d114d1b8021ced6ff21c674cb74b5d
Daniel <30321308+GdanielLS@users.noreply.github.com>
2018-10-08 22:04:54 +0200

    initial

---
+++ src/Test.cpp
@@ -0,0 +1,24 @@
+/*
+ * Dateiname        : ASCII-Tabelle.cpp
+ * Beschreibung     : Darstellung einer ASCII-Tabelle mit 4 Spalten,
+ *                    Die Zahlen werden in Dezimal, - Okatl, - und
+ *                    Hexadezimalzeichen angegeben. Es wird mit "0"
+ *                    aufgefüllt und nicht darstellbare Zeichen werden
+ *                    mit "..." angegeben.
+ * Version          : 1.0
+ * Erstellungsdatum : 08.10.2018    15:00
+ * Compiler         : MinGW GCC
+ * Name des Autors  : Marcel Cauli
+ * Email            : cauli.marcel@fh-swf.de
+ * Organisation     : FH Südwestfalen, Iserlohn
+ */
+#include<iostream>
+#include<iomanip>
+using namespace std;
+//-----------------------------------------------------------------------------------
+// Zählervariable für Schleifen
+int main() {
+
+	cout << "test" << endl;
+	return 0;
+}
